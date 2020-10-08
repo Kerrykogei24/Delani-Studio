c16118ec811654609d37ca180bf05b4786b98e00
Kerrykogei24 <kerrykomar@gmail.com>
2020-10-09 02:19:37 +0300

    added functions on jQuery

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-$("#design img").click(function() {
-    $("#design p").toggle(2000);
-})
-$("#develop img").click(function() {
-    $("#develop p").toggle(2000);
-})
-$("#product img").click(function() {
-    $("#product p").toggle(2000);
-})
+$(".show, .hide").click(function() {
+    $(".hide").slideToggle(2000);
+    $(".show").toggle(1500);
+});
+$(".show1, .hide1").click(function() {
+    $(".hide1").slideToggle(2000);
+    $(".show1").toggle(1500);
+});
+$(".show2, .hide2").click(function() {
+    $(".hide2").slideToggle(2000);
+    $(".show2").toggle(1500);
+});

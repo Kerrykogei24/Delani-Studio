295e2ce8157661b746b051b2b6d8bb0aa7b54a09
Kerrykogei24 <kerrykomar@gmail.com>
2020-10-09 01:09:44 +0300

    added jquery effect

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$("#design img").click(function() {
+    $("#design p").toggle(2000);
+})
+$("#develop img").click(function() {
+    $("#develop p").toggle(2000);
+})
+$("#product img").click(function() {
+    $("#product p").toggle(2000);
+})

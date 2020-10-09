1a1c475b00c868fbf630e0c02815918e08de7537
Kerrykogei24 <kerrykomar@gmail.com>
2020-10-09 12:08:58 +0300

    added hide and show function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
-$(".show, .hide").click(function() {
-    $(".hide").slideToggle(2000);
-    $(".show").toggle(1500);
-});
-$(".show1, .hide1").click(function() {
-    $(".hide1").slideToggle(2000);
-    $(".show1").toggle(1500);
-});
-$(".show2, .hide2").click(function() {
-    $(".hide2").slideToggle(2000);
-    $(".show2").toggle(1500);
+//  $(".show, .hide").click(function() {
+//      $(".hide").slideToggle(2000);
+//      $(".show").toggle(1500);
+//  });
+//  $(".show1, .hide1").click(function() {
+//      $(".hide1").slideToggle(2000);
+//      $(".show1").toggle(1500);
+//  });
+//  $(".show2, .hide2").click(function() {
+//      $(".hide2").slideToggle(2000);
+//      $(".show2").toggle(1500);
+//  });
+
+
+$(document).ready(function() {
+    $(".show").click(function() {
+        $(".show").hide(2000);
+        $(".hide").show(2000);
+    });
+    $(".hide").click(function() {
+        $(".hide").hide(2000);
+        $(".show").show(2000);
+
+    })
+
+
 });
